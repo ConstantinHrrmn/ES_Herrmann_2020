db9fa6116b7b46bb7e66fa4265dae38c2b7db260
ConstantinHrrmn <31920335+ConstantinHrrmn@users.noreply.github.com>
2020-04-02 11:07:53 +0200

    Removing Test Folder

--- tests/react-designer-master/src/actions/Dragger.js
+++
@@ -1,7 +0,0 @@
-export default ({object, startPoint, mouse}) => {
-  return {
-    ...object,
-    x: mouse.x - (startPoint.clientX - startPoint.objectX),
-    y: mouse.y - (startPoint.clientY - startPoint.objectY)
-  };
-};
