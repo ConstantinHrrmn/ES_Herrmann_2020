9a69821c009c1fe67c5abbdc67d34c3a737c095c
ConstantinHrrmn <31920335+ConstantinHrrmn@users.noreply.github.com>
2020-04-07 15:21:54 +0200

    Ajout du template
    Le template que j'ai choisi proviens du site de Themeforest

---
+++ RESA/Template/Documentation/assets/js/main.js
@@ -0,0 +1,20 @@
+//Scroll to section
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+
+$(".ss-nav-item a").click(function(){
+  $(".ss-nav-item a").removeClass("ss-active-doc");
+  $(this).addClass("ss-active-doc");
+});
